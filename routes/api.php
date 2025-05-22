d66974f7647fb8403b34d80cf7445a6f0ee13501
agnescherrlyyy <99299974+agnescherrlyyy@users.noreply.github.com>
2025-05-22 12:11:52 +0700

    uppdate: route api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DetailQrCodeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GeneratePDFController;
+use App\Http\Controllers\GenereteQrCodeController;
 use App\Http\Controllers\SampleTicketPdfController;
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,15 @@ Route::post('/generate-invoice-website-saloka-pdf', [GeneratePDFController::clas
 Route::post('/generate-invoice-livin-sukha-pdf', [GeneratePDFController::class, 'GenerateInvoiceLivinSukha']);
 
 Route::get('/sample-ticket-pdf', [SampleTicketPdfController::class, 'index']);
+
+// Generete QR Code
+Route::post('/generete-qrcode-create', [GenereteQrCodeController::class, 'store'])->name('generete-qrcode-create');
+Route::get('/generete-qrcode', [GenereteQrCodeController::class, 'index'])->name('generete-qrcode');
+Route::get('/generete-qrcode-detail/{id}', [GenereteQrCodeController::class, 'show'])->name('generete-qrcode-detail');
+Route::post('/generete-qrcode-update/{id}', [GenereteQrCodeController::class, 'update'])->name('generete-qrcode-update');
+Route::post('/generete-qrcode-approval/{id}', [GenereteQrCodeController::class, 'approval'])->name('generete-qrcode-approval');
+Route::post('/generete-qrcode-submit/{id}', [GenereteQrCodeController::class, 'submit'])->name('generete-qrcode-submit');
+
+// Detail Qr Code
+Route::post('/qrcode-create/{id}', [DetailQrCodeController::class, 'store'])->name('detail-qrcode-create');
+Route::post('/qrcode-update/{id}', [DetailQrCodeController::class, 'update'])->name('detail-qrcode-update');
